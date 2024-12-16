fae6350a3da06ded3fb5becad0a77450caae30ef
Ruslan Barcho <vinterruslan@gmail.com>
2024-12-16 05:28:06 +0300

    Profile list empty state; Release build gitignore

--- composeApp/src/commonMain/kotlin/pw/vintr/vintrless/presentation/screen/profile/list/ProfileListScreen.kt
+++ composeApp/src/commonMain/kotlin/pw/vintr/vintrless/presentation/screen/profile/list/ProfileListScreen.kt
@@ -1,10 +1,7 @@
 package pw.vintr.vintrless.presentation.screen.profile.list
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,15 +15,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import pw.vintr.vintrless.domain.profile.model.ProfileData
-import pw.vintr.vintrless.presentation.theme.Gilroy16
-import pw.vintr.vintrless.presentation.theme.RubikMedium14
-import pw.vintr.vintrless.presentation.theme.VintrlessExtendedTheme
-import pw.vintr.vintrless.presentation.theme.cardShadow
+import pw.vintr.vintrless.presentation.theme.*
 import pw.vintr.vintrless.presentation.uikit.button.ButtonSecondary
 import pw.vintr.vintrless.presentation.uikit.button.ButtonSecondarySize
 import pw.vintr.vintrless.presentation.uikit.container.RestrictedWidthLayout
@@ -35,11 +30,12 @@ import pw.vintr.vintrless.presentation.uikit.selector.AppRadioButton
 import pw.vintr.vintrless.presentation.uikit.toolbar.ToolbarRegular
 import pw.vintr.vintrless.tools.extensions.Dot
 import pw.vintr.vintrless.tools.extensions.Space
+import vintrless.composeapp.generated.resources.*
 import vintrless.composeapp.generated.resources.Res
 import vintrless.composeapp.generated.resources.action_edit
 import vintrless.composeapp.generated.resources.ic_add
-import vintrless.composeapp.generated.resources.ic_share
 import vintrless.composeapp.generated.resources.ic_delete
+import vintrless.composeapp.generated.resources.ic_share
 import vintrless.composeapp.generated.resources.profile_list_title
 
 @Composable
@@ -82,31 +78,57 @@ fun ProfileListScreen(
                     .fillMaxSize(),
                 state = screenState.value
             ) { state ->
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(24.dp),
-                    contentPadding = PaddingValues(
-                        vertical = 20.dp,
-                        horizontal = 28.dp,
-                    )
-                ) {
-                    items(state.payload.profiles) { profile ->
-                        ProfileCard(
-                            profileData = profile,
-                            selected = profile == state.payload.selectedProfile,
-                            onSelectClick = {
-                                viewModel.selectProfile(profile)
-                            },
-                            onEditClick = {
-                                viewModel.openEditProfile(profile)
-                            },
-                            onShareClick = {
-                                viewModel.onShareClick(profile)
-                            },
-                            onDeleteClick = {
-                                viewModel.onDeleteClick(profile)
-                            }
+                if (state.payload.profiles.isNotEmpty()) {
+                    // Profile items
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(24.dp),
+                        contentPadding = PaddingValues(
+                            vertical = 20.dp,
+                            horizontal = 28.dp,
+                        )
+                    ) {
+                        items(state.payload.profiles) { profile ->
+                            ProfileCard(
+                                profileData = profile,
+                                selected = profile == state.payload.selectedProfile,
+                                onSelectClick = {
+                                    viewModel.selectProfile(profile)
+                                },
+                                onEditClick = {
+                                    viewModel.openEditProfile(profile)
+                                },
+                                onShareClick = {
+                                    viewModel.onShareClick(profile)
+                                },
+                                onDeleteClick = {
+                                    viewModel.onDeleteClick(profile)
+                                }
+                            )
+                        }
+                    }
+                } else {
+                    // Empty state
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 28.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.illustration_profile),
+                            contentDescription = null,
+                        )
+                        Spacer(modifier = Modifier.height(28.dp))
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            text = stringResource(Res.string.no_config_description),
+                            color = VintrlessExtendedTheme.colors.textRegular,
+                            style = RubikMedium16(),
+                            textAlign = TextAlign.Center,
                         )
                     }
                 }
