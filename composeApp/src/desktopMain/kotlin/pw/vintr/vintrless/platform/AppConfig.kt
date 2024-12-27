a8d657496cfa6030a0593efdce76b66df28c76a0
Ruslan Barcho <vinterruslan@gmail.com>
2024-12-27 16:32:04 +0300

    Xray core update (windows)

--- composeApp/src/desktopMain/kotlin/pw/vintr/vintrless/platform/AppConfig.kt
+++ composeApp/src/desktopMain/kotlin/pw/vintr/vintrless/platform/AppConfig.kt
@@ -4,5 +4,5 @@ actual object AppConfig {
 
     actual val appVersionName: String = "Vintrless 1.0.0"
 
-    actual val v2RayCoreVersionName: String = "Xray-core v1.8.241"
+    actual val v2RayCoreVersionName: String = "Xray-core v24.12.18"
 }
